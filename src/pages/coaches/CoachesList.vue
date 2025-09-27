cd764ca76d10df40b0ad655056817e39aa42fcb9
Ritik Negi <negiritik47@gmail.com>
2025-09-27 19:18:34 +0530

    wiring up pages

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,8 +1,10 @@
 <template>
+  <section>FILTER</section>
   <section>
-    FILTER
-  </section>
-  <section>
-    LIST OF COACHES
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul>LIST OF COACHES</ul>
   </section>
 </template>
