70d3d16a46e5b4e46a6523e090dae6aebbe36e7f
Ritik Negi <negiritik47@gmail.com>
2025-09-27 22:49:32 +0530

    rendering loading spinner, http error handling & senindg coaching http requests

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -10,7 +10,7 @@ export default {
     };
 
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/coaches/${userId}.json`,
+      `https://vue-practice-http-aec42-default-rtdb.firebaseio.com//coaches/${userId}.json`,
       {
         method: 'PUT',
         body: JSON.stringify(coachData)
@@ -28,14 +28,19 @@ export default {
       id: userId
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(
-      `https://vue-http-demo-85e9e.firebaseio.com/coaches.json`
+      `https://vue-practice-http-aec42-default-rtdb.firebaseio.com/coaches.json`
     );
     const responseData = await response.json();
 
     if (!response.ok) {
-      // ...
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
     }
 
     const coaches = [];
@@ -53,5 +58,6 @@ export default {
     }
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   }
 };
